b20bb8a70dde2020dcb897a1f5b280babfca0b3c
DesalegnSendek <desalegnsendek2@gmail.com>
2024-02-27 11:18:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
       <>
       <BrowserRouter>
-      <Home/>
+      <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/About' element={<About/>}/>
